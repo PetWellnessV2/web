29aa50cb1faae4481b3d6ccf9e3ede661aef4169
DiegoEBV <diegoballonvi@gmail.com>
2024-11-01 23:27:27 -0500

    fix: arreglo de error de routing

--- src/app/inicio/inicio-routing.module.ts
+++ src/app/inicio/inicio-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './inicio.component';
-import { TareasComponent } from './tareas/tareas.component';
 import { InicioDuenoComponent } from './inicio-dueno/inicio-dueno.component';
+import { InicioVetComponent } from './inicio-vet/inicio-vet.component';
 
 const routes: Routes = [
-  { path: 'inicio-dueno', component: InicioDuenoComponent }
+  { path: 'inicio-dueno', component: InicioDuenoComponent },
+  { path: 'inicio-vet', component: InicioVetComponent }
 ];
 
 @NgModule({
