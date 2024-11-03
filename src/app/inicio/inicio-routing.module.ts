daf23ad29a736f28dc47bf74d6ef638a953a9bbf
DiegoEBV <diegoballonvi@gmail.com>
2024-11-03 14:49:24 -0500

    fix: agregar recordatorios

--- src/app/inicio/inicio-routing.module.ts
+++ src/app/inicio/inicio-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioDuenoComponent } from './inicio-dueno/inicio-dueno.component';
 import { InicioVetComponent } from './inicio-vet/inicio-vet.component';
+import { RecordatoriosComponent } from './recordatorios/recordatorios.component';
 
 const routes: Routes = [
   { path: 'inicio-dueno', component: InicioDuenoComponent },
-  { path: 'inicio-vet', component: InicioVetComponent }
+  { path: 'inicio-vet', component: InicioVetComponent },
+  { path: 'add-reminder', component: RecordatoriosComponent },
 ];
 
 @NgModule({
