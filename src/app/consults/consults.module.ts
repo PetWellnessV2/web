d5031e427fcf7b269e191fe1a5245d3dce042d96
aleec02 <conzahualpa@gmail.com>
2024-11-06 15:29:03 -0500

    feat(informe-vet): Implementar frontend de registro de informe de veterinario y visualizacion del informe + postConsulta

--- src/app/consults/consults.module.ts
+++ src/app/consults/consults.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ConsultsRoutingModule } from './consults-routing.module';
 import { ConsultsComponent } from './consults.component';
 import { VeterinarioComponent } from './veterinario/veterinario.component';
 import { DuenoComponent } from './dueno/dueno.component';
 import { ConsultasComponent } from './consultas/consultas.component';
 import { FormsModule } from '@angular/forms';
-
+import { MatIconModule } from '@angular/material/icon';
+import { ConsultasVetComponent } from './consultas-vet/consultas-vet.component';
+import { InformeComponent } from './informe/informe.component';
+import { PostConsultaComponent } from './post-consulta/post-consulta.component';
 
 @NgModule({
   declarations: [
     ConsultsComponent,
     VeterinarioComponent,
     DuenoComponent,
-    ConsultasComponent
+    ConsultasComponent,
+    ConsultasVetComponent,
+    InformeComponent,
+    PostConsultaComponent
   ],
   imports: [
     CommonModule,
     ConsultsRoutingModule,
-    FormsModule
+    FormsModule,
+    MatIconModule
   ]
 })
 export class ConsultsModule { }
