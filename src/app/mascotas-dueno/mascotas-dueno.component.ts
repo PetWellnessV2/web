ab5eb44a19d20666d6af002cc58d07539cd808c7
Marsi3116 <marsi3116@gmail.com>
2024-11-06 21:51:43 -0500

    feat: Vista vets

--- src/app/mascotas-dueno/mascotas-dueno.component.ts
+++ src/app/mascotas-dueno/mascotas-dueno.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ConsultasService, MascotaResponse } from '../services/consultas.service';
 import { Router } from '@angular/router';
 
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './mascotas-dueno.component.html',
   styleUrl: './mascotas-dueno.component.css'
 })
-export class MascotasDuenoComponent {
+export class MascotasDuenoComponent implements OnInit{
   mascotas: MascotaResponse[] = [];
 
   constructor(private consultaService: ConsultasService, private router: Router) {}
